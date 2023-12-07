648f9429a4251595da8e48073c4c1359554726a1
Dmytro <moiseenkodmitriy@i.ua>
2023-12-07 19:19:42 +0200

    Updates

---
+++ assets/DriverStartingPage-6d889ca5.js
@@ -0,0 +1,20 @@
+import{s,_ as c,j as i}from"./index-b8763b14.js";import{a as e}from"./Home.styled-e6bc2e74.js";const d=s.h2`
+  font-weight: 600;
+  margin-bottom: 28px;
+  font-size: 32px;
+  line-height: 1.2;
+  margin-top: 0;
+  text-align: center;
+  text-shadow: 2px 2px 4px #000000;
+`,p=s.img`
+  width: 600px;
+  object-fit: contain;
+  object-position: center;
+  display: flex;
+  margin: 16px auto;
+`,l=s.div`
+  width: 1280px;
+  padding: 32px 16px;
+  margin: 0 auto;
+  text-align: center;
+`,g="/mnvk/assets/driver-85091077.png",h=t=>{t&&t instanceof Function&&c(()=>import("./web-vitals-60d3425a.js"),[]).then(({getCLS:o,getFID:r,getFCP:a,getLCP:n,getTTFB:x})=>{o(t),r(t),a(t),n(t),x(t)})},v=()=>i.jsxs(l,{children:[i.jsx(d,{children:"Водій автотранспортних засобів (категорії «В», «С»)"}),i.jsx(e,{to:"/driver/about",children:"Про професію"}),i.jsx(e,{to:"/driver/gallery",children:"Галерея"}),i.jsx(e,{to:"/driver/lec",children:"Лекційний матеріал"}),i.jsx(e,{to:"/driver/test",children:"Атестація"}),i.jsx(p,{src:g,alt:"Водій автотранспортних засобів"})]});h();export{v as default};

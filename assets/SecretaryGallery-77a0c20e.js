4390734d3033e96db0fea661edae7e60638e46ef
Dmytro <moiseenkodmitriy@i.ua>
2023-12-07 17:23:13 +0200

    Updates

---
+++ assets/SecretaryGallery-77a0c20e.js
@@ -0,0 +1,36 @@
+import{s as t,j as i,L as r}from"./index-f523ec91.js";const s=t.button`
+  text-decoration: none;
+  margin: 0 6px;
+  color: white;
+  transition: all 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+  border: 1px solid #1677ff;
+  border-radius: 2px;
+  box-shadow: 0 2px 0 rgb(5 145 255 / 10%);
+  padding: 2px 8px;
+  background-color: #1677ff;
+  font-weight: 400;
+  font-size: 18px;
+
+  &:hover,
+  &:focus {
+    opacity: 0.8;
+    background-color: white;
+    color: #1677ff;
+  }
+`,p=t.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 18px;
+  padding: 16px;
+`,n=t.img`
+  width: 300px;
+  height: 200px;
+  object-fit: contain;
+  object-position: center;
+  display: flex;
+  margin: 0 auto;
+`,c=t.div`
+  width: 1280px;
+  padding: 32px 16px;
+  margin: 0 auto;
+`,x=()=>{const a=["/public/images/Секретарі/1.jpg","/public/images/Секретарі/2.jpg","/public/images/Секретарі/3.JPG","/public/images/Секретарі/4.JPG","/public/images/Секретарі/5.jpg","/public/images/Секретарі/4.JPG"];return i.jsxs(c,{children:[i.jsx(r,{to:"/secretary",children:i.jsx(s,{type:"button",children:"Go back"})}),i.jsx(p,{children:a.map((e,o)=>i.jsx(n,{src:e,alt:`Image ${o}`},o))})]})};export{x as default};
---
+++ assets/SecretaryStartingPage-34a913bc.js
@@ -0,0 +1,20 @@
+import{s,_ as x,j as e}from"./index-f523ec91.js";import{h as p}from"./secretary-3b68a9f8.js";import{a as i}from"./Home.styled-dd6e5197.js";const l=s.h2`
+  font-weight: 600;
+  margin-bottom: 28px;
+  font-size: 32px;
+  line-height: 1.2;
+  margin-top: 0;
+  text-align: center;
+  text-shadow: 2px 2px 4px #000000;
+`,d=s.img`
+  width: 600px;
+  object-fit: contain;
+  object-position: center;
+  display: flex;
+  margin: 16px auto;
+`,h=s.div`
+  width: 1280px;
+  padding: 32px 16px;
+  margin: 0 auto;
+  text-align: center;
+`,m=t=>{t&&t instanceof Function&&x(()=>import("./web-vitals-60d3425a.js"),[]).then(({getCLS:o,getFID:a,getFCP:r,getLCP:c,getTTFB:n})=>{o(t),a(t),r(t),c(t),n(t)})},_=()=>e.jsxs(h,{children:[e.jsx(l,{children:"Секретар керівника (організації підприємства, установи)"}),e.jsx(i,{to:"/secretary/about",children:"Про професію"}),e.jsx(i,{to:"/secretary/gallery",children:"Галерея"}),e.jsx(i,{to:"/secretary/lec",children:"Лекційний матеріал"}),e.jsx(i,{to:"/secretary/test",children:"Атестація"}),e.jsx(d,{src:p,alt:"Секретар керівника"})]});m();export{_ as default};

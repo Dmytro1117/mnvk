228bde91384e4a5f9af777172dcbba6b5ef14caf
Dmytro <moiseenkodmitriy@i.ua>
2023-12-07 16:45:12 +0200

    deploy

--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/mmnvk">
+        <BrowserRouter basename="/mnvk">
           <App />
         </BrowserRouter>
       </PersistGate>

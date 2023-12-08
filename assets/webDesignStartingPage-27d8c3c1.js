97cfc8fbfe15dba3f16f9f5e4708d53b38194ff8
Dmytro <moiseenkodmitriy@i.ua>
2023-12-08 13:18:47 +0200

    Updates

---
+++ assets/webDesignStartingPage-27d8c3c1.js
@@ -0,0 +1,20 @@
+import{s as i,j as t}from"./index-03735fe3.js";import{h as o}from"./web-b556bf39.js";import{a as e}from"./Home.styled-44b8c216.js";import{r}from"./LocksmithreportWebVitals-1d1068a9.js";const n=i.h2`
+  font-weight: 600;
+  margin-bottom: 28px;
+  font-size: 32px;
+  line-height: 1.2;
+  margin-top: 0;
+  text-align: center;
+  text-shadow: 2px 2px 4px #000000;
+`,s=i.img`
+  width: 600px;
+  object-fit: contain;
+  object-position: center;
+  display: flex;
+  margin: 16px auto;
+`,a=i.div`
+  width: 1280px;
+  padding: 32px 16px;
+  margin: 0 auto;
+  text-align: center;
+`,m=()=>t.jsxs(a,{children:[t.jsx(n,{children:"Основи web-дизайну та web-програмування"}),t.jsx(e,{to:"/web/about",children:"Про професію"}),t.jsx(e,{to:"/web/gallery",children:"Галерея"}),t.jsx(e,{to:"/web/lec",children:"Лекційний матеріал"}),t.jsx(e,{to:"/web/test",children:"Атестація"}),t.jsx(s,{src:o,alt:"Основи web-дизайну та web-програмування"})]});r();export{m as default};

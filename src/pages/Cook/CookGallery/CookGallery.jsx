6126b0fcba53f2bc25d25cc14d65b6b351d4b645
Dmytro <moiseenkodmitriy@i.ua>
2023-12-08 17:44:43 +0200

    deploy

--- src/pages/Cook/CookGallery/CookGallery.jsx
+++ src/pages/Cook/CookGallery/CookGallery.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Container, Button, Img, Gallery } from './CookGallery.styled';
 import { Link } from 'react-router-dom';
+// import cook from '../../../../public/images/CookImag';
 
 const CookGallery = () => {
   const imagePaths = [
-    '/public/images/Кухар/1.jpg',
-    '/public/images/Кухар/2.jpg',
-    '/public/images/Кухар/3.jpg',
-    '/public/images/Кухар/4.jpg',
-    '/public/images/Кухар/5.jpg',
-    '/public/images/Кухар/6.jpg',
-    '/public/images/Кухар/7.jpg',
-    '/public/images/Кухар/8.jpg',
-    '/public/images/Кухар/9.jpg',
-    '/public/images/Кухар/10.jpg',
-    '/public/images/Кухар/11.jpg',
-    '/public/images/Кухар/12.jpg',
-    '/public/images/Кухар/13.jpg',
-    '/public/images/Кухар/14.jpg',
-    '/public/images/Кухар/15.jpg',
-    '/public/images/Кухар/16.jpg',
-    '/public/images/Кухар/17.jpg',
-    '/public/images/Кухар/18.jpg',
-    '/public/images/Кухар/19.jpg',
+    '../../../../public/images/CookImag/1.jpg',
+    '../../../../public/images/CookImag/2.jpg',
+    '../../../../public/images/CookImag/3.jpg',
+    '../../../../public/images/CookImag/4.jpg',
+    '../../../../public/images/CookImag/5.jpg',
+    '../../../../public/images/CookImag/6.jpg',
+    '../../../../public/images/CookImag/7.jpg',
+    '../../../../public/images/CookImag/8.jpg',
+    '../../../../public/images/CookImag/9.jpg',
+    '../../../../public/images/CookImag/10.jpg',
+    '../../../../public/images/CookImag/11.jpg',
+    '../../../../public/images/CookImag/12.jpg',
+    '../../../../public/images/CookImag/13.jpg',
+    '../../../../public/images/CookImag/14.jpg',
+    '../../../../public/images/CookImag/15.jpg',
+    '../../../../public/images/CookImag/16.jpg',
+    '../../../../public/images/CookImag/17.jpg',
+    '../../../../public/images/CookImag/18.jpg',
+    '../../../../public/images/CookImag/19.jpg',
   ];
 
   return (

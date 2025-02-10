3afd587c32e3d98e048d91d4b1de4af0419b30e9
Dmytro <moiseenkodmitriy@i.ua>
2025-02-10 15:45:41 +0200

    deploy

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import hero from '../../images/hero.jpg';
+// import hero from '../../images/hero.jpg';
 
 export const Tex = styled.p`
   text-align: center;
